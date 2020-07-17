c508ba3bc9236d3d3f02c289e3069e7672a7cf9a
Devedse <davydavidse@gmail.com>
2020-07-17 14:26:25 +0200

    Added usings

--- DeveUnityLicenseActivator/LicenseActivator.cs
+++ DeveUnityLicenseActivator/LicenseActivator.cs
@@ -39,86 +39,90 @@ namespace DeveUnityLicenseActivator
                 args = new string[] { "--no-sandbox", "--disable-setuid-sandbox" };
             }
 
-            var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions
             {
                 Headless = !cliOptions.ShowWindow,
                 Args = args,
-            });
-
-            Console.WriteLine("Opening page...");
-
-            var page = await browser.NewPageAsync();
-            await page.SetViewportAsync(new ViewPortOptions() { Width = 1280, Height = 1024 });
-            await page.GoToAsync(Constants.UnityLicenseUrl);
-
-            try
+            }))
             {
-                //Login
-                await page.WaitForSelectorAsync("#conversations_create_session_form_email");
-                await page.WaitForSelectorAsync("#conversations_create_session_form_password");
-                Console.WriteLine("Logging in...");
 
-                await page.TypeAsync("#conversations_create_session_form_email", cliOptions.Email, slowerTypeOptions);
-                await page.TypeAsync("#conversations_create_session_form_password", cliOptions.Password, slowerTypeOptions);
+                Console.WriteLine("Opening page...");
 
-                await page.ClickAsync("#new_conversations_create_session_form input[value='Sign in']");
-
-                //Upload file
-                await page.WaitForSelectorAsync("#licenseFile");
-                Console.WriteLine("Uploading file...");
-
-                var fileChooserTask = page.WaitForFileChooserAsync();
-                await page.ClickAsync("#licenseFile");
-
-                var fileChooser = await fileChooserTask;
-
-                await fileChooser.AcceptAsync(cliOptions.LicenseFile);
-
-                await page.ClickAsync("input[value='Next']");
-
-                //Activate your license
-                var unityPersonalEditionButton = await page.WaitForSelectorAsync("label[for='type_personal']");
-                Console.WriteLine("Selecting edition...");
-
-                await unityPersonalEditionButton.ClickAsync();
-
-                var notUseUnityInProfessionalCapacity = await page.WaitForSelectorAsync("label[for='option3']");
-                await notUseUnityInProfessionalCapacity.ClickAsync();
-
-                var nextButton = await page.WaitForSelectorAsync(".selected input[value='Next']");
-                await nextButton.ClickAsync();
-
-                //Download license file
-                await page.WaitForSelectorAsync("input[value='Download license file']");
-                Console.WriteLine("Downloading license file...");
-
-                var downloadManager = new DownloadManager(Directory.GetCurrentDirectory());
-                await downloadManager.SetupPageAsync(page);
-
-
-                await page.ClickAsync("input[value='Download license file']");
-                var response = await page.WaitForResponseAsync(r => r.Url.Equals($"https://license.unity3d.com/genesis/activation/download-license", StringComparison.OrdinalIgnoreCase));
-
-                var data = await response.JsonAsync();
-                var xmlData = data["xml"].ToString();
-                var fileName = data["name"].ToString();
-
-                File.WriteAllText("Unity_lic.ulf", xmlData);
-
-                Console.WriteLine($"File 'Unity_lic.ulf' created. Size: {new FileInfo("Unity_lic.ulf").Length}");
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex}");
-                var ssstream = await page.ScreenshotStreamAsync();
-
-                using (var fileStream = File.Create("error.png"))
+                using (var page = await browser.NewPageAsync())
                 {
-                    ssstream.Seek(0, SeekOrigin.Begin);
-                    ssstream.CopyTo(fileStream);
+                    await page.SetViewportAsync(new ViewPortOptions() { Width = 1280, Height = 1024 });
+                    await page.GoToAsync(Constants.UnityLicenseUrl);
+
+                    try
+                    {
+                        //Login
+                        await page.WaitForSelectorAsync("#conversations_create_session_form_email");
+                        await page.WaitForSelectorAsync("#conversations_create_session_form_password");
+                        Console.WriteLine("Logging in...");
+
+                        await page.TypeAsync("#conversations_create_session_form_email", cliOptions.Email, slowerTypeOptions);
+                        await page.TypeAsync("#conversations_create_session_form_password", cliOptions.Password, slowerTypeOptions);
+
+                        await page.ClickAsync("#new_conversations_create_session_form input[value='Sign in']");
+
+                        //Upload file
+                        await page.WaitForSelectorAsync("#licenseFile");
+                        Console.WriteLine("Uploading file...");
+
+                        var fileChooserTask = page.WaitForFileChooserAsync();
+                        await page.ClickAsync("#licenseFile");
+
+                        var fileChooser = await fileChooserTask;
+
+                        await fileChooser.AcceptAsync(cliOptions.LicenseFile);
+
+                        await page.ClickAsync("input[value='Next']");
+
+                        //Activate your license
+                        var unityPersonalEditionButton = await page.WaitForSelectorAsync("label[for='type_personal']");
+                        Console.WriteLine("Selecting edition...");
+
+                        await unityPersonalEditionButton.ClickAsync();
+
+                        var notUseUnityInProfessionalCapacity = await page.WaitForSelectorAsync("label[for='option3']");
+                        await notUseUnityInProfessionalCapacity.ClickAsync();
+
+                        var nextButton = await page.WaitForSelectorAsync(".selected input[value='Next']");
+                        await nextButton.ClickAsync();
+
+                        //Download license file
+                        await page.WaitForSelectorAsync("input[value='Download license file']");
+                        Console.WriteLine("Downloading license file...");
+
+                        var downloadManager = new DownloadManager(Directory.GetCurrentDirectory());
+                        await downloadManager.SetupPageAsync(page);
+
+
+                        await page.ClickAsync("input[value='Download license file']");
+                        var response = await page.WaitForResponseAsync(r => r.Url.Equals($"https://license.unity3d.com/genesis/activation/download-license", StringComparison.OrdinalIgnoreCase));
+
+                        var data = await response.JsonAsync();
+                        var xmlData = data["xml"].ToString();
+                        var fileName = data["name"].ToString();
+
+                        File.WriteAllText("Unity_lic.ulf", xmlData);
+
+                        Console.WriteLine($"File 'Unity_lic.ulf' created. Size: {new FileInfo("Unity_lic.ulf").Length}");
+                        return 0;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex}");
+                        var ssstream = await page.ScreenshotStreamAsync();
+
+                        using (var fileStream = File.Create("error.png"))
+                        {
+                            ssstream.Seek(0, SeekOrigin.Begin);
+                            ssstream.CopyTo(fileStream);
+                        }
+                        return 1;
+                    }
                 }
-                return 1;
             }
         }
     }
